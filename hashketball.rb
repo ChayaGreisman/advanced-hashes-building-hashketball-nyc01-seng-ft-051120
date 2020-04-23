b9ead8731c2294e39cf738c57b43201e93d2b406
Chaya Greisman <chayamgreisman@gmail.com>
2020-04-23 04:19:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,21 +1,34 @@
 # Write your code here!
 def game_hash
   { :home => {
-      team_name:
-      colors:
-      players:
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {player_name: "Alan Anderson",
+         number: 0,
+         shoe: 16,
+         points: 22,
+         rebounds: 12,
+         assists: 12,
+         steals: 3,
+         blocks: ,
+         slam_dunks: }
+        ]
     }
   
     :away => {
       team_name: "",
-      colors: []
+      colors: [],
       players: [
         {player_name:},
-        {number: }
-        {shoe: }
-        {points: }
-        {rebounds: }
-        {assists: }
+        {number: },
+        {shoe: },
+        {points: },
+        {rebounds: },
+        {assists: },
+        {steals: },
+        {blocks: },
+        {slam_dunks: }
         ]
     }
   }
